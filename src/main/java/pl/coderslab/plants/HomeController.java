9b32d6fd613750f49193ca142fa3a965669bd235
Marcin Łukaszewski <lukaszewski.marcin@gazeta.pl>
2019-04-28 15:07:49 +0200

    view resolver configuration

--- src/main/java/pl/coderslab/plants/HomeController.java
+++ src/main/java/pl/coderslab/plants/HomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "home.html";
+        return "home";
     }
 }
